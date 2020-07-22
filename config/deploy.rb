a86c7a4746d286c908c05bd3c8b2966aad20de8b
k-kit-san <ldolfu1107@gmail.com>
2020-07-22 16:19:26 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.14.1"
 
 set :application, "fleamarket_sample_79e"
-set :repo_url, "git@example.com:k-kit-san/fleamarket_sample_79e.git"
+set :repo_url, "git@github.com:k-kit-san/fleamarket_sample_79e.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
