a2332c29d8dc92689266b0225374a0ee8014510b
akane-m <a2mt824nv@gmail.com>
2020-07-29 10:11:48 +0900

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'items#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :confirms, only: :index
+  resources :details, only: :index
+  resources :mypages, only: :index
+  resources :cards, only: [:index, :new]
+  resources :logs, only: :index
 end
