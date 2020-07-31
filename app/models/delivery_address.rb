341e9ade34eedaac7de3b0c084bb311de33a87e1
k-kit-san <ldolfu1107@gmail.com>
2020-07-31 18:30:14 +0900

    errore

--- app/models/delivery_address.rb
+++ app/models/delivery_address.rb
@@ -1,7 +1,7 @@
 class DeliveryAddress < ApplicationRecord
   belongs_to :user, optional: true
   validates :delivery_family_name, :delivery_first_name, :delivery_family_name_kana, :delivery_first_name_kana, :post_code, :city, :home_number, presence: true
-  validates :post_code, format: {/^\d{3}[-]\d{4}$/}
+  # validates :post_code, format: {/^\d{3}[-]\d{4}$/}
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
