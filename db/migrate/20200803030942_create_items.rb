8ccbe948e1c9d0df802b82209ef934eb0dbb6fca
akane-m <a2mt824nv@gmail.com>
2020-08-03 15:22:18 +0900

    itemモデルおよびテーブルの作成

---
+++ db/migrate/20200803030942_create_items.rb
@@ -0,0 +1,16 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name, null: false
+      t.text :explanation, null: false
+      t.references :category, null: false, foreign_key: true
+      t.string :brand
+      t.integer :condition_id, null: false
+      t.integer :postage_id, null: false
+      t.integer :prefecture_id, null: false
+      t.integer :prepare_id, null: false
+      t.integer :price, null: false
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_03_030238) do
+ActiveRecord::Schema.define(version: 2020_08_03_030942) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -40,6 +40,21 @@ ActiveRecord::Schema.define(version: 2020_08_03_030238) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "explanation", null: false
+    t.bigint "category_id", null: false
+    t.string "brand"
+    t.integer "condition_id", null: false
+    t.integer "postage_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "prepare_id", null: false
+    t.integer "price", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_items_on_category_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -54,4 +69,5 @@ ActiveRecord::Schema.define(version: 2020_08_03_030238) do
   end
 
   add_foreign_key "delivery_addresses", "users"
+  add_foreign_key "items", "categories"
 end
