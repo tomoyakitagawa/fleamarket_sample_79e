fc792e960ebdb97806a491cf83c12d5df40b7a71
akane-m <a2mt824nv@gmail.com>
2020-08-03 17:08:22 +0900

    associationの記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  has_many :item_images, dependent: :destroy
+  belongs_to :category
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :postage
+  belongs_to_active_hash :prepare
 end
