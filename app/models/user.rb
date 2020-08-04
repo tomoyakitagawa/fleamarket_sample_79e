7bed0f89f3ac8694fcd3608e8caa65e1e5256d43
nigorihonoka <ganache2526@gmail.com>
2020-08-04 19:56:36 +0900

    バリデーションを整理

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+  validates :nickname, :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { in: 7..12 }
   # 漢字と全角の平仮名とカタカナのバリデーション
   VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
-  validates :family_name, presence: true, format: { with: VALID_NAME_REGEX }
-  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX }
+  validates :family_name, :first_name, presence: true, format: { with: VALID_NAME_REGEX }
   # 全角カタカナのバリデーション
   VALID_KANA_NAME_REGEX = /\A[ァ-ヶー－]+\z/
-  validates :family_name_kana, presence: true, format: { with: VALID_KANA_NAME_REGEX }
-  validates :first_name_kana, presence: true, format: { with: VALID_KANA_NAME_REGEX }
+  validates :family_name_kana, :first_name_kana, presence: true, format: { with: VALID_KANA_NAME_REGEX }
+
   has_one :delivery_address
 end
