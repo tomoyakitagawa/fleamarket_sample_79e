e2c01f3e08269c67076f3d369af4a2f22cdb83bc
tomoyakitagawa <tomoyakitagawa0930@gmail.com>
2020-08-04 19:56:33 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
-Rails.application.routes.draw d品出
-  root 'dispray#index'
-  devise_for users
+Rails.application.routes.draw do 
+  # root 'dispray#index'
+  # devise_for users
   devise_for :users, controllers: {
     registrations: 'users/registrations',
   }
