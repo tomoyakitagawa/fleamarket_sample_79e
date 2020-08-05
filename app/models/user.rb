858e848119019e1ff98287fe482cee28d584e8c3
nigorihonoka <ganache2526@gmail.com>
2020-08-05 12:23:54 +0900

    バリデーションの設定を変更と、エラーメッセージ表現の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { in: 7..12 }
+  validates :password, presence: true, length: { minimum: 7 }
   # 漢字と全角の平仮名とカタカナのバリデーション
   VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
-  validates :family_name, :first_name, presence: true, format: { with: VALID_NAME_REGEX }
+  validates :family_name, :first_name, presence: true, format: { with: VALID_NAME_REGEX, message: "は全角で入力してください" }
   # 全角カタカナのバリデーション
   VALID_KANA_NAME_REGEX = /\A[ァ-ヶー－]+\z/
-  validates :family_name_kana, :first_name_kana, presence: true, format: { with: VALID_KANA_NAME_REGEX }
+  validates :family_name_kana, :first_name_kana, presence: true, format: { with: VALID_KANA_NAME_REGEX, message: "は全角カナで入力してください" }
 
   has_one :delivery_address
 end
