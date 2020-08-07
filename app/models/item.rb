9843bd7edc88d9e2346fabefe5875e475df9b673
akane-m <a2mt824nv@gmail.com>
2020-08-07 11:17:17 +0900

    カテゴリー機能の実装-モデル修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   has_many :item_images
+  belongs_to :category
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :condition
   belongs_to_active_hash :postage
@@ -10,9 +11,6 @@ class Item < ApplicationRecord
 
   validates :name, length: { maximum: 40 }, presence: true
   validates :explanation, length: { maximum: 1000 }, presence: true
-  validates :condition_id, :postage_id, :prefecture_id, :prepare_id, presence: true
+  validates :category_id, :condition_id, :postage_id, :prefecture_id, :prepare_id, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, presence: true
-
-  #下記は、後ほどcategoryモデルとアソシエーションを組むときに使用します
-  #belongs_to :category
 end
