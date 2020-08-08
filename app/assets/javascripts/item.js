f62de7d6db34e6f93916c3cdc9b9e35728ffe30e
akane-m <a2mt824nv@gmail.com>
2020-08-08 18:11:09 +0900

    不要なコメントアウトの削除

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,110 +1,97 @@
-$(function(){
-  //================================================================
-  //共通の定数を定義
-  //==================================================================
-  // label-contentの直前に配置されている要素(今回はdiv class="prev-content")を取得する
-  
-  const prevContent = $('.label-content').prev();
+$(document).on('turbolinks:load', function(){
+  $(function(){
 
-  //============================================================
-  //プレビューのhtmlを定義
-  //============================================================
-  function buildHTML(id,image) {
-    var html = `<div class="preview-box">
-                  <div class="upper-box">
-                    <img src=${image} alt="preview">
-                  </div>
-                  <div class="lower-box">
-                    <div class="delete-box">
-                      <div class="delete-btn" data-delete-id= ${id}>削除</div>
+    // 共通の定数を定義---------------------------------------------
+    // label-contentの直前に配置されている要素(今回はdiv class="prev-content")を取得する
+    const prevContent = $('.label-content').prev();
+
+    // プレビューのhtmlを定義---------------------------------------------
+    function buildHTML(id,image) {
+      var html = `<div class="preview-box">
+                    <div class="upper-box">
+                      <img src=${image} alt="preview">
                     </div>
-                  </div>
-                </div>`
-    return html;
-  }
+                    <div class="lower-box">
+                      <div class="delete-box">
+                        <div class="delete-btn" data-delete-id= ${id}>削除</div>
+                      </div>
+                    </div>
+                  </div>`
+      return html;
+    }
 
-  //==================================================
-  //ラベルのwidth・id・forの値を変更
-  //==================================================
-  function setLabel(count) {
-    //プレビューが5個あったらラベルを隠す
-    if (count == 5) { 
-      $('.label-content').hide();
-    } else {
-      //プレビューが4個以下の場合はラベルを表示
-      $('.label-content').show();
+    // ラベルのwidth・id・forの値を変更---------------------------------------------
+
+    function setLabel(count) {
+      //プレビューが5個あったらラベルを隠す
+      if (count == 5) { 
+        $('.label-content').hide();
+      } else {
+        //プレビューが4個以下の場合はラベルを表示
+        $('.label-content').show();
+        //プレビューボックスのwidthを取得し、maxから引くことでラベルのwidthを決定(prev-contentのwidthを取って、大枠(width:620px)から引き算する)
+        labelWidth = (620 - parseInt($(prevContent).css('width')));  //parseIntは文字列を整数に変換するjavascriptの関数
+        $('.label-content').css('width', labelWidth);
+        //id・forの値を変更
+        $('.label-box').attr({for: `item_item_images_attributes_${count}_image`});
+      }
+    }
+    //画像を削除した時は下記
+    function setLabelDelete(id) {
       //プレビューボックスのwidthを取得し、maxから引くことでラベルのwidthを決定(prev-contentのwidthを取って、大枠(width:620px)から引き算する)
       labelWidth = (620 - parseInt($(prevContent).css('width')));  //parseIntは文字列を整数に変換するjavascriptの関数
       $('.label-content').css('width', labelWidth);
       //id・forの値を変更
-      $('.label-box').attr({for: `item_item_images_attributes_${count}_image`});
+      $('.label-box').attr({for: `item_item_images_attributes_${id}_image`});
     }
-  }
-  //画像を削除してから再投稿した時はこっち
-  function setLabelAAA(id) {
-    //プレビューは4個以下のはずなのでラベルは表示
-    //プレビューボックスのwidthを取得し、maxから引くことでラベルのwidthを決定(prev-contentのwidthを取って、大枠(width:620px)から引き算する)
-    labelWidth = (620 - parseInt($(prevContent).css('width')));  //parseIntは文字列を整数に変換するjavascriptの関数
-    $('.label-content').css('width', labelWidth);
-    //id・forの値を変更
-    $('.label-box').attr({for: `item_item_images_attributes_${id}_image`});
-  }
 
-  //====================================================
-  //編集ページ(items/:i/edit)へリンクした際のアクション
-  //====================================================
-  if (window.location.href.match(/\/items\/\d+\/edit/)){
-    //プレビューの数を取得
-    var count = $('.preview-box').length;
-    //countに応じてラベルのwidth・id・forの値を変更
-    setLabel(count) 
-  }
-
-  //=======================================================
-  //プレビューの追加
-  //=======================================================
-  //hidden-fieldsの値が変更したとき発火
-  $(document).on('change', '.hidden-field', function() {
-    console.log("hidden-fieldの変更イベントが発火している");
-    //発火したhidden-fieldのidの数値のみ取得
-    var id = $(this).attr('id').replace(/[^0-4]/g, '');
-    //選択したfileのオブジェクトを取得
-    var file = this.files[0];
-    // PC内にあるファイルをアプリケーションに非同期で読み込む
-    var reader = new FileReader();
-    //readAsDataURLで指定したFileオブジェクトを読み込む
-    reader.readAsDataURL(file);
-    //読み込み時に発火するイベント
-    reader.onload = function() {
-      // 直前に実行されたイベント（imageファイルの読み込み）を変数imageに代入
-      var image = this.result;
-      //htmlを作成
-      var html = buildHTML(id,image);
-      //ラベルの直前のプレビュー群にプレビューを追加
-      $(prevContent).append(html);
+    // 編集ページ(items/:i/edit)へリンクした際のアクション---------------------------------------------
+    if (window.location.href.match(/\/items\/\d+\/edit/)){
       //プレビューの数を取得
       var count = $('.preview-box').length;
       //countに応じてラベルのwidth・id・forの値を変更
-      setLabel(count);
+      setLabel(count) 
     }
-  });
 
-  //=====================================================================
-  // 画像の削除
-  //=====================================================================
-  $(document).on('click', '.delete-btn', function() {
-    var id = $(this).attr('data-delete-id')
-    //削除用チェックボックスにチェックを入れる
-    if ($(`#item_item_images_attributes_${id}__destroy`).length) {
-      $(`#item_item_images_attributes_${id}__destroy`).prop('checked',true);
-    }
-    //画像を消去
-    $(this).parent().parent().parent().remove();
-    //フォームの中身を削除(.hidden-fieldの該当箇所が、ファイル名から選択されていませんに変化)
-     $(`#item_item_images_attributes_${id}_image`).val("");
-    //プレビューの数を取得
-    //var count = $('.preview-box').length;
-    //idに応じてラベルのwidth・id・forの値を変更
-    setLabelAAA(id);
+    // プレビューの追加---------------------------------------------
+    //hidden-fieldsの値が変更したとき発火
+    $(document).on('change', '.hidden-field', function() {
+      //発火したhidden-fieldのidの数値のみ取得
+      var id = $(this).attr('id').replace(/[^0-4]/g, '');
+      //選択したfileのオブジェクトを取得
+      var file = this.files[0];
+      // PC内にあるファイルをアプリケーションに非同期で読み込む
+      var reader = new FileReader();
+      //readAsDataURLで指定したFileオブジェクトを読み込む
+      reader.readAsDataURL(file);
+      //読み込み時に発火するイベント
+      reader.onload = function() {
+        // 直前に実行されたイベント（imageファイルの読み込み）を変数imageに代入
+        var image = this.result;
+        //htmlを作成
+        var html = buildHTML(id,image);
+        //ラベルの直前のプレビュー群にプレビューを追加
+        $(prevContent).append(html);
+        //プレビューの数を取得
+        var count = $('.preview-box').length;
+        //countに応じてラベルのwidth・id・forの値を変更
+        setLabel(count);
+      }
     });
+
+    // 画像の削除---------------------------------------------
+    $(document).on('click', '.delete-btn', function() {
+      var id = $(this).attr('data-delete-id')
+      //削除用チェックボックスにチェックを入れる
+      if ($(`#item_item_images_attributes_${id}__destroy`).length) {
+        $(`#item_item_images_attributes_${id}__destroy`).prop('checked',true);
+      }
+      //画像を消去
+      $(this).parent().parent().parent().remove();
+      //フォームの中身を削除(.hidden-fieldの該当箇所が、ファイル名から選択されていませんに変化)
+      $(`#item_item_images_attributes_${id}_image`).val("");
+      //idに応じてラベルのwidth・id・forの値を変更
+      setLabelDelete(id);
+    });
+  });
 });
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,9 +7,9 @@ class ItemsController < ApplicationController
     @item = Item.new
     @item.item_images.new
 
-    #セレクトボックスの初期値設定
+    # セレクトボックスの初期値設定
     @category_parent_array = ["---"]
-    #データベースから、親カテゴリーのみ抽出し、配列化
+    # データベースから、親カテゴリーのみ抽出し、配列化
     @category_parent_array = Category.where(ancestry: nil)
   end
 
@@ -36,14 +36,13 @@ class ItemsController < ApplicationController
   # 以下全て、formatはjsonのみ
   # 親カテゴリーが選択された後に動くアクション
   def get_category_children
-    #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
-    # ここでfind_byを使うことでレディースしか取れてなかった
+    # 選択された親カテゴリーに紐付く子カテゴリーの配列を取得
     @category_children = Category.find(params[:parent_id]).children
   end
 
   # 子カテゴリーが選択された後に動くアクション
   def get_category_grandchildren
-    #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
+    # 選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
     @category_grandchildren = Category.find(params[:child_id]).children
   end
 
