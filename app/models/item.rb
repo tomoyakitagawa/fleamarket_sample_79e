667981cbc9692254926ca4e9507c1c188c0be1c5
akane-m <a2mt824nv@gmail.com>
2020-08-09 22:12:00 +0900

    itemモデルのテスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,12 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   has_many :item_images, dependent: :destroy
-  belongs_to :category
+  belongs_to :category, optional: true
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :condition
   belongs_to_active_hash :postage
   belongs_to_active_hash :prepare
-  belongs_to :seller, class_name: 'User', foreign_key: 'seller_id'
+  belongs_to :seller, class_name: 'User', foreign_key: 'seller_id', optional: true
   belongs_to :buyer, class_name: 'User', foreign_key: 'buyer_id', optional: true
 
   accepts_nested_attributes_for :item_images, allow_destroy: true
