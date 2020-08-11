5821401fd4209f27ff4246960429da53d6e94f47
k-kit-san <ldolfu1107@gmail.com>
2020-08-11 20:13:47 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @nickname = current_user.nickname
-    @item = Item.where(seller_id: current_user.id).length
+    @user_items_count = Item.where(seller_id: current_user.id).length
   end
 end
