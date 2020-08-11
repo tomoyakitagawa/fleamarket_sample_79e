06b78a27056221df4e12bc465d3660a586745d5c
akane-m <a2mt824nv@gmail.com>
2020-08-12 01:32:18 +0900

    itemモデルの変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,13 +11,8 @@ class Item < ApplicationRecord
 
   accepts_nested_attributes_for :item_images, allow_destroy: true
 
-  enum status: {
-    sale: 1,
-    sold: 2
-  }
-
   validates :name, length: { maximum: 40 }, presence: true
   validates :explanation, length: { maximum: 1000 }, presence: true
-  validates :status, :category_id, :condition_id, :postage_id, :prefecture_id, :prepare_id, :seller_id, presence: true
+  validates :category_id, :condition_id, :postage_id, :prefecture_id, :prepare_id, :seller_id, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, presence: true
 end
