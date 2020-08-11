da32f2cb3f0be641ba5d4977e89137478e7f8448
nigorihonoka <ganache2526@gmail.com>
2020-08-11 22:37:52 +0900

    editとupdateアクションの追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,15 +25,15 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.includes(:item_images).find(params[:id])
-
+    @item = Item.includes(:item_images).order('created_at DESC').find(params[:id])
+    
     grandchild_category = @item.category
     child_category = grandchild_category.parent
 
 
     @category_parent_array = []
     Category.where(ancestry: nil).each do |parent|
-      @category_parent_array << parent.name
+      @category_parent_array << parent
     end
 
     @category_children_array = []
@@ -45,10 +45,10 @@ class ItemsController < ApplicationController
     Category.where(ancestry: grandchild_category.ancestry).each do |grandchildren|
       @category_grandchildren_array << grandchildren
     end
-
   end
 
   def update
+    
     @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path
@@ -74,5 +74,6 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :explanation, :brand, :category_id, :condition_id, :postage_id, :prefecture_id, :prepare_id, :price, item_images_attributes: [:image, :_destroy, :id]).merge(seller_id: current_user.id)
+    
   end
 end
